461cf3d50c3ba281586018552b034cc74ae72949
NFLD99 <NFLD99@users.noreply.github.com>
2024-11-30 02:02:52 -0330

    Update

--- _Fluffums/Editor/OcclusionCubeGeneratorWindow.cs
+++ _Fluffums/Editor/OcclusionCubeGeneratorWindow.cs
@@ -108,7 +108,7 @@ public class OcclusionCubeGeneratorWindow : EditorWindow
 
         string[] cubeNames = { "Right", "Left", "Top", "Bottom", "Front", "Back" };
 
-        Material occlusionMaterial = AssetDatabase.LoadAssetAtPath<Material>("Assets/_Fluffums/Mats/-- Occlusion Mat --");
+        Material occlusionMaterial = AssetDatabase.LoadAssetAtPath<Material>("Assets/_Fluffums/Mats/-- Occlusion Mat --.mat");
         if (occlusionMaterial == null)
         {
             Debug.LogError("Occlusion material not found.");
